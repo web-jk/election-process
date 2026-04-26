9384b4eec1699daeb330df2d3bdbeaf4e600a336
Pankajkumar <createwebwithjk@gmail.com>
2026-04-26 12:24:29 +0530

    security: remove exposed API key from tracking, add firebase.config.ts to .gitignore

--- src/app/firebase.config.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyBE7dleUNXrlQOLiWzRlMV1pPfX7aDskVI",
-  authDomain: "election-process-4a0cd.firebaseapp.com",
-  projectId: "election-process-4a0cd",
-  storageBucket: "election-process-4a0cd.firebasestorage.app",
-  messagingSenderId: "1052182103369",
-  appId: "1:1052182103369:web:a6f7c1d5df08ae2476c698",
-  measurementId: "G-C14YSYCRCH"
-};
